c55effa2ed0fe5c2966367be37abd665dab27512
senol <simsek2.senol@gmail.com>
2021-11-12 15:11:14 +0300

    first save

---
+++ Trend_analysis/support_and_resistance.py
@@ -0,0 +1,20 @@
+
+
+#1
+#how can we determine support and resistance level
+
+#in series of close prices we need to determine lower lows and higher highs
+#and then we need to determine close price hit the trendline or not
+#we called it a line but it can be area if we determine area also we need to determine how close price interact in these area
+
+#2
+#secondly we need to find pre-determined patterns
+
+
+#3
+#and last one using indicators to get another tips for coin nest movements
+#all of the above methodes must work for all cryptocoins with the asyncronous way and later maybe for stocks
+
+
+
+#these 3 stages and my personal ides will create our trading stretagie
