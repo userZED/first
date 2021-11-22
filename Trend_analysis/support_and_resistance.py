66b982da6114c7567fc7ea5b8d8acc1d7fa1be32
senol <simsek2.senol@gmail.com>
2021-11-22 16:39:19 +0300

    second

--- Trend_analysis/support_and_resistance.py
+++ Trend_analysis/support_and_resistance.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+import numpy as np
 
 #1
 #how can we determine support and resistance level
@@ -18,3 +19,17 @@
 
 
 #these 3 stages and my personal ides will create our trading stretagie
+solana_daily = pd.read_csv("SOL1-USD.csv")
+close = np.array(solana_daily["Close"][-100:])
+sorted_close=close
+sorted_close.sort()
+
+#wee need to determine how much x1 and x2 are close each other 
+y1 = sorted_close[-2]
+x1 = int(np.where(close == y1)[0])
+
+y2 = sorted_close[-1]
+x2 = int(np.where(close == y2)[0])
+
+resistance_line_slope = (y2-y1)/(x2-x1)
+
